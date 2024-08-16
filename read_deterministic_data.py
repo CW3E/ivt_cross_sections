85cc09dc1c7e6c508ab3a67e9962a1579574d718
Deanna Nash <dnash@ucsd.edu>
2024-08-16 11:51:10 -0700

    start adding ECMWF data, swap figure to non-transparent, change winds to u and v, and swap wind units to knots)

--- read_deterministic_data.py
+++ read_deterministic_data.py
@@ -77,6 +77,134 @@ def read_gfs_deterministic(filename, vardict, show_catalog=False):
 
     return selected_vars
 
+def read_ecmwf_S2D(filename, vardict, show_catalog=False):
+    
+    '''
+    author: Ricardo Vilela
+    email: rbatistavilela@ucsd.edu
+
+    function usage:
+    
+    filename example:
+    S2D04151200041515001.grb
+
+    vardict example:
+
+    vardict = {
+               
+                "u_wind":{"shortName":'u'}, #U-component of wind
+                "v_wind":{"shortName":'v'}, #V-component of wind
+                "temperature":{"shortName":'t'}, #Temperature
+                "specific_humidity":{"shortName":'q'}, #Specific humidity
+                
+                
+                }
+    Output:
+    Dictionary of objects for each variable set in the vardict argument. Ex.
+    uwind = selected_vars["u_wind"].values
+    uwind_latitude = selected_vars["u_wind"].latitude.values
+    uwind_longitude = selected_vars["u_wind"].longitude.values
+
+    These files are in model levels instead of pressure levels, in other words, pressure is not a dimension but an extra variable built based on a lookup table of coefficients and the hybrid dimension.
+
+    retrieving the pressure array
+    pressure_array = selected_vars["pressure"].values
+
+
+    '''
+    
+    print('[INFO] reading ECMWF file: '+filename)    
+
+
+    selected_vars = {}
+    
+    # reads surface pressure first (this field is needed in order to calculate the pressure levels for each grid)
+    sfc_pressure_ds = xr.open_dataset(filename,
+                        engine='cfgrib',filter_by_keys={"shortName":'lnsp'})
+    
+
+    
+    sfc_pressure = 2.71828**sfc_pressure_ds.lnsp.values
+
+    #read the coefficient lookup table
+    coeff = pd.read_csv("utils/ecmwf_coeffs.txt",names=["A","B"],sep=" ")
+    coeffA = np.array(coeff["A"])[:, np.newaxis, np.newaxis]
+    coeffB = np.array(coeff["B"])[:, np.newaxis, np.newaxis]
+
+    #creating the pressure based on the coefficient table and the surface pressure
+    pressure = (coeffA + (sfc_pressure * coeffB))/100
+
+    #iterating over all variables in the vardict and storing each one in a new dictionary called selected_vars
+    for var in vardict.keys():
+    
+        ds = xr.open_dataset(filename,
+                        engine='cfgrib',filter_by_keys=vardict[var])
+
+        selected_vars[var] = ds[vardict[var]["shortName"]]
+
+
+        
+    ngrids = pressure.shape[0]*pressure.shape[1]*pressure.shape[2]
+    
+    #creating pressure 3d data array and its attributes
+    pressure = xr.DataArray(pressure, name="pressure", dims=("hybrid", "latitude","longitude"), coords={"hybrid": coeff.index, "latitude":sfc_pressure_ds.latitude.values, "longitude":sfc_pressure_ds.longitude.values, "time":sfc_pressure_ds.time.values, "valid_time":sfc_pressure_ds.valid_time.values})
+    pressure.attrs = {'GRIB_paramId': 0, 'GRIB_dataType': 'fc', 'GRIB_numberOfPoints': ngrids, 'GRIB_typeOfLevel': 'hybrid', 'GRIB_stepUnits': 1, 'GRIB_stepType': 'instant', 'GRIB_gridType': 'regular_ll', 'GRIB_NV': 276, 'GRIB_Nx': len(pressure.longitude.values), 'GRIB_Ny': len(pressure.latitude.values), 'GRIB_Nz': len(pressure.hybrid.values), 'GRIB_cfName': 'unknown', 'GRIB_cfVarName': 'pressure', 'GRIB_gridDefinitionDescription': 'Latitude/longitude', 'GRIB_iDirectionIncrementInDegrees': 0.1, 'GRIB_iScansNegatively': 0, 'GRIB_jDirectionIncrementInDegrees': 0.1, 'GRIB_jPointsAreConsecutive': 0, 'GRIB_jScansPositively': 0, 'GRIB_latitudeOfFirstGridPointInDegrees': np.nanmax(pressure.latitude.values), 'GRIB_latitudeOfLastGridPointInDegrees': np.nanmin(pressure.latitude.values), 'GRIB_longitudeOfFirstGridPointInDegrees': np.nanmin(pressure.longitude.values), 'GRIB_longitudeOfLastGridPointInDegrees': np.nanmax(pressure.longitude.values), 'GRIB_missingValue': 3.4028234663852886e+38, 'GRIB_name': 'Grid wise atmospheric pressure', 'GRIB_shortName': 'pressure', 'GRIB_units': 'Numeric', 'long_name': 'Atmospheric Pressure', 'units': 'hPa', 'standard_name': 'unknown'}
+    
+    
+    #adding pressure variable to the object dictionary
+    selected_vars["pressure"] = pressure
+    
+
+    #standardize latitude longitude time and level dimension name and position in the array
+
+    return selected_vars
+
+
+def read_ecmwf_S1D(filename, vardict, show_catalog=False):
+    
+    '''
+    author: Ricardo Vilela
+    email: rbatistavilela@ucsd.edu
+
+    function usage:
+    
+    filename example:
+    S1D04151200041515001
+
+    vardict example:
+
+    vardict = {
+               
+               "msl_pressure":{"shortName":'msl'}, #msl pressure
+                
+                
+                }
+    Output:
+    Dictionary of objects for each variable set in the vardict argument. Ex.
+    mslp = selected_vars["mslp"].values
+
+
+    '''
+    
+    print('[INFO] reading ECMWF file: '+filename)
+        
+    selected_vars = {}
+
+    #iterating over all variables in the vardict and storing each one in a new dictionary called selected_vars
+    for var in vardict.keys():
+    
+        ds = xr.open_dataset(filename,
+                        engine='cfgrib',filter_by_keys=vardict[var])
+
+        if var == 'u_wind_10m':
+            selected_vars[var] = ds["u10"]
+        elif var == 'v_wind_10m':
+            selected_vars[var] = ds["v10"]
+        else:    
+            selected_vars[var] = ds[vardict[var]["shortName"]]   
+        
+    return selected_vars
+
 class load_GFS_datasets:
     '''
     Loads variables needed for ivt cross section plots from GFS grb files
@@ -121,7 +249,6 @@ class load_GFS_datasets:
         gfs_vardict = {
         "u_wind":{"typeOfLevel":'isobaricInhPa',"shortName":"u"}, #U-component of wind
         "v_wind":{"typeOfLevel":'isobaricInhPa',"shortName":"v"}, #V-component of wind
-        "w_wind":{"typeOfLevel":'isobaricInhPa',"shortName":"w"}, #W-component of wind
         "iwv":{"typeOfLevel":'atmosphereSingleLayer',"shortName":"pwat"}, #Integrated precipitable water or IWV
         "temperature":{"typeOfLevel":'isobaricInhPa',"shortName":"t"}, #Temperature
         "rh":{"typeOfLevel":'isobaricInhPa',"shortName":"r"}, #Relative Humidity
@@ -154,7 +281,6 @@ class load_GFS_datasets:
         ## 3D vars
         u_wind_da = xr.DataArray(gfs["u_wind"].values, name="u", dims=("pressure", "latitude","longitude"), coords={"pressure": gfs["u_wind"].isobaricInhPa.values, "latitude": gfs["u_wind"].latitude.values, "longitude":gfs["u_wind"].longitude.values})
         v_wind_da = xr.DataArray(gfs["v_wind"].values, name="v", dims=("pressure", "latitude","longitude"), coords={"pressure": gfs["v_wind"].isobaricInhPa.values, "latitude": gfs["v_wind"].latitude.values, "longitude":gfs["v_wind"].longitude.values})
-        w_wind_da = xr.DataArray(gfs["w_wind"].values, name="w", dims=("pressure", "latitude","longitude"), coords={"pressure": gfs["w_wind"].isobaricInhPa.values, "latitude": gfs["w_wind"].latitude.values, "longitude":gfs["w_wind"].longitude.values})
         wvflux_da = xr.DataArray(wv_flux, name="wvflux", dims=("pressure", "latitude","longitude"), coords={"pressure": gfs["v_wind"].isobaricInhPa.values, "latitude": gfs["v_wind"].latitude.values, "longitude":gfs["v_wind"].longitude.values})
 
 
@@ -171,7 +297,6 @@ class load_GFS_datasets:
                                             'freezing_level':freezing_level_da,
                                             'u':u_wind_da,
                                             'v':v_wind_da,
-                                            'w':w_wind_da,
                                             'wvflux':wvflux_da}, 
                                 attrs={"model":"GFS", "init":gfs["sfc_pressure"].time.values, "valid_time":gfs["sfc_pressure"].valid_time.values, "datacrs":ccrs.PlateCarree(central_longitude=0)}
                                 ) 
