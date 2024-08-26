5af50223f1337bd894e1cc43a90b1cf34a357da0
Deanna Nash <dnash@ucsd.edu>
2024-08-26 16:11:18 -0700

    work toward making operational

--- plotter.py
+++ plotter.py
@@ -1,40 +1,207 @@
 #!/usr/bin/python3
 """
-Filename:    plot_cross_section.py
-Author:      Deanna Nash, dnash@ucsd.edu
-Description: function to plot ivt cross sections for GFS and ECMWF deterministic data
+Filename:    plotter.py
+Author:      Deanna Nash, dnash@ucsd.edu and Ricardo Vilela, rbatistavilela@ucsd.edu
+Description: Functions for plotting IVT cross sections
 """
-## basic modules
-import os
-import sys
-import re
-import xarray as xr
-import glob
-import numpy as np
-import pandas as pd
-import itertools ## need this for the cbarticks
-import textwrap
-import datetime
 
-## plotting modules
-import cartopy
+import os, sys
+import numpy as np
+import xarray as xr
 import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-from matplotlib.colorbar import Colorbar # different way to handle colorbar
-import matplotlib.ticker as mticker
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import cmocean.cm as cmo
-import cmocean
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+import matplotlib.ticker as mticker
+import colorsys
+from datetime import datetime, timedelta
+from matplotlib.colors import LinearSegmentedColormap # Linear interpolation for color maps
+import matplotlib.patches as mpatches
+from matplotlib import cm, colors as clr
+from matplotlib.colorbar import Colorbar # different way to handle colorbar
+import pandas as pd
+from matplotlib.gridspec import GridSpec
+import itertools
+from PIL import Image
+from matplotlib import font_manager as fm
+from matplotlib.ticker import FuncFormatter
+from scipy.ndimage import gaussian_filter
+import copy
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
-## personal modules
-from plotter import draw_basemap, plot_cw3e_logo, set_cw3e_font
-import domains
-import cw3ecmaps as ccmaps
+import matplotlib as mpl
+mpl.use('agg')
 
-def plot_ivt_cross_sections(ds, cross, current_line, model_name, F):
+import cw3ecmaps as ccmaps
+import domains as domains
+
+def roundPartial(value, resolution):
+    return np.round(value / resolution) * resolution
+
+def set_cw3e_font(current_dpi, scaling_factor):
+    fm.fontManager.addfont('/home/cw3eit/ARPortal/gefs/scripts/ar_landfall_tool/utils/fonts/helvetica.ttc')
+
+    plt.rcParams.update({
+                    'font.family' : 'Helvetica',
+                    'figure.dpi': current_dpi,
+                    'font.size': 8 * scaling_factor, #changes axes tick label
+                    'axes.labelsize': 8 * scaling_factor,
+                    'axes.titlesize': 8 * scaling_factor,
+                    'xtick.labelsize': 8 * scaling_factor,#do nothing
+                    'ytick.labelsize': 8 * scaling_factor, #do nothing
+                    'legend.fontsize': 5 * scaling_factor,
+                    'lines.linewidth': 0.7 * scaling_factor,
+                    'axes.linewidth': 0.2 * scaling_factor,
+                    'legend.fontsize': 12 * scaling_factor,
+                    'xtick.major.width': 0.8 * scaling_factor,
+                    'ytick.major.width': 0.8 * scaling_factor,
+                    'xtick.minor.width': 0.6 * scaling_factor,
+                    'ytick.minor.width': 0.6 * scaling_factor,
+                    'lines.markersize': 6 * scaling_factor
+                })
+
+def plot_cw3e_logo(ax, orientation):
+    ## location of CW3E logo
+    if orientation == 'horizontal':
+        im = '/common/CW3E_Logo_Suite/1-Horzontal-PRIMARY_LOGO/Digital/JPG-RGB/CW3E-Logo-Horizontal-FullColor-RGB.jpg'
+    else:
+        im = '/common/CW3E_Logo_Suite/5-Vertical-Acronym_Only/Digital/PNG/CW3E-Logo-Vertical-Acronym-FullColor.png'
+    img = np.asarray(Image.open(im))
+    ax.imshow(img)
+    ax.axis('off')
+    return ax
+
+
+def draw_basemap(ax, datacrs=ccrs.PlateCarree(), extent=None, xticks=None, yticks=None, grid=False, left_lats=True, right_lats=False, bottom_lons=True, mask_ocean=False, coastline=True):
+    """
+    Creates and returns a background map on which to plot data. 
+    
+    Map features include continents and country borders.
+    Option to set lat/lon tickmarks and draw gridlines.
+    
+    Parameters
+    ----------
+    ax : 
+        plot Axes on which to draw the basemap
+    
+    datacrs : 
+        crs that the data comes in (usually ccrs.PlateCarree())
+        
+    extent : float
+        Set map extent to [lonmin, lonmax, latmin, latmax] 
+        Default: None (uses global extent)
+        
+    grid : bool
+        Whether to draw grid lines. Default: False
+        
+    xticks : float
+        array of xtick locations (longitude tick marks)
+    
+    yticks : float
+        array of ytick locations (latitude tick marks)
+        
+    left_lats : bool
+        Whether to add latitude labels on the left side. Default: True
+        
+    right_lats : bool
+        Whether to add latitude labels on the right side. Default: False
+        
+    Returns
+    -------
+    ax :
+        plot Axes with Basemap
+    
+    Notes
+    -----
+    - Grayscale colors can be set using 0 (black) to 1 (white)
+    - Alpha sets transparency (0 is transparent, 1 is solid)
+    
+    """
+    ## some style dictionaries
+    kw_ticklabels = {'color': 'black', 'weight': 'light'}
+    kw_grid = {'linewidth': 0.6, 'color': 'k', 'linestyle': '--', 'alpha': 0.4}
+    kw_ticks = {'length': 4, 'width': 0.5, 'pad': 2, 'color': 'black',
+                             'labelcolor': 'dimgray'}
+
+    # Use map projection (CRS) of the given Axes
+    mapcrs = ax.projection    
+    
+    # Add map features (continents and country borders)
+    ax.add_feature(cfeature.LAND, facecolor='0.9')      
+    ax.add_feature(cfeature.BORDERS, edgecolor='0.4', linewidth=0.8)
+    ax.add_feature(cfeature.STATES, edgecolor='0.2', linewidth=0.2)
+    if coastline == True:
+        ax.add_feature(cfeature.COASTLINE, edgecolor='0.4', linewidth=0.8)
+    if mask_ocean == True:
+        ax.add_feature(cfeature.OCEAN, edgecolor='0.4', zorder=12, facecolor='white') # mask ocean
+        
+    ## Tickmarks/Labels
+    ## Add in meridian and parallels
+    if mapcrs == ccrs.NorthPolarStereo(central_longitude=0.):
+        gl = ax.gridlines(draw_labels=False,
+                      linewidth=.5, color='black', alpha=0.5, linestyle='--')
+    elif mapcrs == ccrs.SouthPolarStereo(central_longitude=0.):
+        gl = ax.gridlines(draw_labels=True,
+                      linewidth=.5, color='black', alpha=0.5, linestyle='--')
+        
+    else:
+        gl = ax.gridlines(crs=datacrs, draw_labels=True, **kw_grid)
+        gl.top_labels = False
+        gl.left_labels = left_lats
+        gl.right_labels = right_lats
+        gl.bottom_labels = bottom_lons
+        gl.xlocator = mticker.FixedLocator(xticks)
+        gl.ylocator = mticker.FixedLocator(yticks)
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+        gl.xlabel_style = kw_ticklabels
+        gl.ylabel_style = kw_ticklabels
+        
+        # apply tick parameters
+        ax.set_xticks(xticks, crs=datacrs)
+        ax.set_yticks(yticks, crs=datacrs)
+        plt.yticks(color='w', size=1) # hack: make the ytick labels white so the ticks show up but not the labels
+        plt.xticks(color='w', size=1) # hack: make the ytick labels white so the ticks show up but not the labels
+        ax.ticklabel_format(axis='both', style='plain')
+    
+    ## Gridlines
+    # Draw gridlines if requested
+    if (grid == True):
+        gl.xlines = True
+        gl.ylines = True
+    if (grid == False):
+        gl.xlines = False
+        gl.ylines = False
+
+    ## Map Extent
+    # If no extent is given, use global extent
+    if extent is None:        
+        ax.set_global()
+        extent = [-180., 180., -90., 90.]
+    # If extent is given, set map extent to lat/lon bounding box
+    else:
+        ax.set_extent(extent, crs=datacrs)
+    
+    return ax
+
+def add_subregion_boxes(ax, subregion_xy, width, height, ecolor, datacrs):
+    '''This function will add subregion boxes to the given axes.
+    subregion_xy 
+    [[ymin, xmin], [ymin, xmin]]
+    '''
+    for i in range(len(subregion_xy)):
+        ax.add_patch(mpatches.Rectangle(xy=subregion_xy[i], width=width[i], height=height[i],
+                                        fill=False,
+                                        edgecolor=ecolor,
+                                        linewidth=1.0,
+                                        transform=datacrs,
+                                        zorder=100))
+        
+    return ax
+
+
+def plot_ivt_cross_sections(ds, cross, line_lst, current_line, model_name, F):
     '''
     Plots IVT Cross Section Plot from given data
     
@@ -84,10 +251,8 @@ def plot_ivt_cross_sections(ds, cross, current_line, model_name, F):
     clon = ((current_lon + 180) % 360) - 180
     if clon > 0:
         lon_lbl = u"{:.0f}\N{DEGREE SIGN}E".format(clon)
-        flon_lbl = u"{:.0f}E".format(clon)
     else:
         lon_lbl = u"{:.0f}\N{DEGREE SIGN}W".format(clon*-1)
-        flon_lbl = u"{:.0f}W".format(clon)
 
     wvflux_units = 'kg m$^{-2}$ s$^{-1}$'
     ivt_units = 'kg m$^{-1}$ s$^{-1}$'
@@ -124,7 +289,7 @@ def plot_ivt_cross_sections(ds, cross, current_line, model_name, F):
 
     fig = plt.figure(figsize=(13.0, 5))
     fig.dpi = current_dpi
-    fname = 'figs/{0}/Cross_Section_latest_25-65N_{1}W_F{2}'.format(model_name, flon_lbl, str(F).zfill(3))
+    fname = 'figs/{0}/Cross_Section_latest_25-65N_{1}W_F{2}'.format(model_name, clon, str(F).zfill(3))
     fmt = 'png'
 
     ####################
@@ -202,12 +367,12 @@ def plot_ivt_cross_sections(ds, cross, current_line, model_name, F):
 
     # wind vectors
     if model_name == 'ECMWF':
-        dw = 8 # how often to plot vector vertically
-        dw2 = 15 # how often to plot horizontally
+        dw = 10 # how often to plot vector vertically
+        dw2 = 17 # how often to plot horizontally
         ax.barbs(x[::dw, ::dw2], ys[::dw, ::dw2], cross.u.values[::dw, ::dw2]*1.94384, cross.v.values[::dw, ::dw2]*1.94384, 
                  linewidth=0.25, length=3.5)
     elif model_name == 'GFS':
-        dw = 5 # how often to plot vector
+        dw = 6 # how often to plot vector
         ax.barbs(xs[::dw], ys[::dw], cross.u.values[::dw, ::dw]*1.944, cross.v.values[::dw, ::dw]*1.944, 
                  linewidth=0.25, length=3.5)
 
--- run_tool.py
+++ run_tool.py
@@ -1,6 +1,57 @@
-## TODO 
-## write a loop for the F and the clon
-## then run plot_cross_section.py using those values
-## for each lead (F):
-## want to load the data
-## then loop each cross section
+"""
+Filename:    run_tool.py
+Author:      Deanna Nash, dnash@ucsd.edu
+Description: For GEFS, and ECMWF: output .png files of ivt cross section plot for different logitudinal cross sections and lead times.
+"""
+import os
+import sys
+import numpy as np
+
+from read_deterministic_data import load_GFS_datasets, load_ECMWF_datasets
+from plotter import plot_ivt_cross_sections
+
+model_name = sys.argv[1]
+print('Creating ivt cross sections for {0}'.format(model_name))
+
+## FIRST LOOP - LEAD TIME ##
+F_lst = np.arange(6, 132, 12)
+
+## create list that has the start and end points for the cross section
+lon_lst = np.arange(170., 245., 5.)
+line_lst = []
+for i, lon in enumerate(lon_lst):
+    ## create list of lines where [start_lat, start_lon, end_lat, end_lon]
+    line = [25., lon, 65., lon]
+    line_lst.append(line)
+
+for i, F in enumerate(F_lst):
+
+    #######################
+    ### LOAD MODEL DATA ###
+    #######################
+    print('... Loading {0} data for {1} hour lead'.format(model_name, F)
+    if model_name == 'ECMWF':
+        s = load_ECMWF_datasets(F=F, fdate='2024073000')
+        model_data= s.calc_vars()
+
+    elif model_name == 'GFS':
+        s = load_GFS_datasets(F=F, fdate='2024072212')
+        model_data= s.calc_vars()
+        
+    ## SECOND LOOP - LOOP THROUGH LONGITUDE FOR CROSS SECTION ##
+    for k, current_line in enumerate(line_lst):
+        ## subset vertical data and IVT data to current line
+        if model_name == 'ECMWF':
+            cross = model_data.sel(latitude = slice(current_line[2]+0.1, current_line[0]-0.1))
+            cross = cross.sel(longitude=current_line[1], method='nearest')
+        elif model_name == 'GFS':
+            cross = model_data.sel(latitude = slice(current_line[2], current_line[0]), longitude=current_line[1])
+
+        cross = cross.sortby('latitude')
+        
+        ### Create Plots
+        print('...... Creating figure for {0}'.format(current_line[1])
+        plot_ivt_cross_sections(model_data, cross, line_lst, current_line, model_name, F)
+        
+        
+    
